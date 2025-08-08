e70da29411b8990b27ce49faa924334490902a3d
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-08-07 21:14:21 -0300

    correccion a los controladores

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -50,37 +50,69 @@ export const createUser = async (req, res) => {
   } catch (error) {
     res
       .status(500)
-      .json({ message: "error al crear el personaje", error: error.message });
+      .json({ message: "error al crear el Usuario", error: error.message });
   }
 };
 
 export const updateUser = async (req, res) => {
   const { id } = req.params;
-  const user = User.findByPk(id);
-  if (!user) {
-    return res.status(404).json({ message: "el personaje no existe" });
-  }
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return "";
-  }
+  const user = await User.findByPk(id);
+
   try {
-  } catch (error) {}
+    if (!user) {
+      return res.status(404).json({ message: "el usuario no existe" });
+    }
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "Los campos no pueden estar vacíos",
+      });
+    }
+
+    const verificarEmail = await User.findOne({ where: { email } });
+    if (verificarEmail && verificarEmail.id !== user.id) {
+      return res
+        .status(400)
+        .json({ message: "ya existe un usuario con este email" });
+    }
+
+    if (name !== undefined && typeof name !== "string") {
+      return res.status(400).json({
+        message: "el nombre solamente puede ser string.",
+      });
+    }
+    if (email !== undefined && typeof email !== "string") {
+      return res.status(400).json({
+        message: "el email solamente puede ser string",
+      });
+    }
+
+    if (name != undefined) user.name = name;
+    if (email != undefined) user.email = email;
+    if (password != undefined) user.password = password;
+    await user.save();
+    res
+      .status(200)
+      .json({ message: "usuario actualizado correctamente", user });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "error al editar el usuario", error: error.message });
+  }
 };
 
 export const deleteUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const user = User.findByPk(id);
-    if (!id) {
-      return res
-        .status(400)
-        .json({ message: "El personaje a borrar no existe" });
+    const user = await User.findByPk(id);
+    if (!user) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
     await user.destroy();
+    res.status(200).json({ message: "Usuario eliminado correctamente" });
   } catch (error) {
     res
       .status(500)
-      .json({ message: "Error al borrar el personaje", error: error.message });
+      .json({ message: "Error al borrar el usuario", error: error.message });
   }
 };
