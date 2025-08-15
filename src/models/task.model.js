612044d2f2d97e452fca507ab7046030a50e14ba
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-08-15 16:31:13 -0300

    Se realizo las relaciones de tasks con los users

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,10 +1,20 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
+import { user_model } from "./user.model.js";
+export const task_model = sequelize.define(
+  "Task",
+  {
+    title: { type: DataTypes.STRING(100), unique: true, allowNull: false },
+    description: { type: DataTypes.STRING(100), allowNull: false },
+    isComplete: { type: DataTypes.BOOLEAN, defaultValue: false },
+  },
+  {
+    timestamps: false,
+  }
+);
 
-const Task = sequelize.define("Task", {
-  title: { type: DataTypes.STRING(100), unique: true, allowNull: false },
-  description: { type: DataTypes.STRING(100), allowNull: false },
-  isComplete: { type: DataTypes.BOOLEAN, defaultValue: false },
-});
+//una tarea pertenece a un unico usuario
+task_model.belongsTo(user_model, { foreignKey: "user_id", as: "responsible" });
 
-export default Task;
+// un usuario tiene muchas tareas
+user_model.hasMany(task_model, { foreignKey: "user_id" });
