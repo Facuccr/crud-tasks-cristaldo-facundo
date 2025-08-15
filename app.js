88d09668e8f7040082a8d941dd851ffdb2be2508
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-08-15 16:50:14 -0300

    se creo la ruta para obtener todas las tareas con los usuarios que los creo

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
+import { initDB } from "./src/config/database.js";
 import express from "express";
 import dotenv from "dotenv";
-import { initDB } from "./src/config/database.js";
 
 import userRouter from "./src/routes/user.routes.js";
-import taskRouter from "./src/routes/task.route.js";
+import taskRouter from "./src/routes/task.routes.js";
+import { task_model } from "./src/models/task.model.js";
+import { user_model } from "./src/models/user.model.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+
+app.get("api/tasks/user_id", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+app.get("/api/tasks", async (req, res) => {
+  try {
+    const tasks = await task_model.findAll({
+      attributes: {
+        exclude: ["user_id", "isComplete"],
+      },
+      include: [
+        {
+          model: user_model,
+          as: "responsible",
+          attributes: ["id", "name", "email"],
+        },
+      ],
+    });
+
+    return res.json(tasks);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error al obtener las tareas.",
+      error,
+    });
+  }
+});
 app.use("/api/users", userRouter);
 app.use("/api/tasks", taskRouter);
 
