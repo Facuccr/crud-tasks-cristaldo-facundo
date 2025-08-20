fb6257c416831b56d521c62252bc8bbf465368e3
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-08-19 21:37:42 -0300

    se agrego la validacion para verificar el rol del usuario al asignarle uno

--- src/controllers/user_role.controller.js
+++ src/controllers/user_role.controller.js
@@ -5,23 +5,29 @@ import { UserRole } from "../models/user_role.model.js";
 export const createUserRole = async (req, res) => {
   try {
     const { user_id, role_id } = req.body;
-
+    //verificar que el rol realmente exista en la bd
     const verificarRole = await role_model.findOne({ where: { id: role_id } });
     if (!verificarRole) {
       return res.status(400).json({ message: "el rol no existe" });
     }
+
+    //verificar que el usuario exista
     const verificarUsuario = await user_model.findOne({
       where: { id: user_id },
     });
-    if (verificarRole) {
-      return res
-        .status(400)
-        .json({ message: "ya se a asignado este rol al usuario especificado" });
-    }
     if (!verificarUsuario) {
       return res
         .status(400)
         .json({ message: "no se encontro el usuario con el id proporsionado" });
+    }
+    //verificar que el usuario no tenga ese rol asignado previamente
+    const existeRelacion = await UserRole.findOne({
+      where: { user_id: user_id, role_id: role_id },
+    });
+    if (existeRelacion) {
+      return res.status(400).json({
+        message: "ese rol ya fue asignado a este usuario",
+      });
     }
     const relation = await UserRole.create({
       user_id: user_id,
