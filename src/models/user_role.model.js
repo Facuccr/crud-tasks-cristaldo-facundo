5a2f4440d10730a8b5bdfa0e7e453e1e69f6c667
facucris <facundocristaldo146@gmail.com>
2025-08-20 12:32:21 -0300

    agregue la columna id a la tabla de relacion entre usuarios y modelos

--- src/models/user_role.model.js
+++ src/models/user_role.model.js
@@ -4,16 +4,22 @@ import sequelize from "../config/database.js";
 export const UserRole = sequelize.define(
   "user_roles",
   {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     user_id: {
       type: DataTypes.INTEGER,
-      primaryKey: true,
+      allowNull: false,
     },
     role_id: {
       type: DataTypes.INTEGER,
-      primaryKey: true,
+      allowNull: false,
     },
   },
   {
     timestamps: false,
   }
 );
+
