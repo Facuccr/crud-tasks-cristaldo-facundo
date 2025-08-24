b2952e523811dcf61bf895ef934592b1bf56e8e3
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-08-24 19:33:25 -0300

    se modificaron las rutas con las validaciones correspondientes

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -2,11 +2,46 @@ import { Router } from "express";
 import {
   createProfile,
   getAllProfiles,
+  getProfileById,
+  updateProfile,
+  deleteProfile,
 } from "../controllers/profile.controller.js";
+
+import {
+  createProfileValidations,
+  updateProfileValidations,
+  profileIdParamValidation,
+} from "../middlewares/validations/profile.validations.js";
+
+import { applyValidations } from "../middlewares/validations.js";
 
 const profileRouter = Router();
 
-profileRouter.post("/", createProfile);
 profileRouter.get("/", getAllProfiles);
+profileRouter.get(
+  "/:id",
+  profileIdParamValidation,
+  applyValidations,
+  getProfileById
+);
+profileRouter.post(
+  "/",
+  createProfileValidations,
+  applyValidations,
+  createProfile
+);
+profileRouter.put(
+  "/:id",
+  profileIdParamValidation,
+  updateProfileValidations,
+  applyValidations,
+  updateProfile
+);
+profileRouter.delete(
+  "/:id",
+  profileIdParamValidation,
+  applyValidations,
+  deleteProfile
+);
 
 export default profileRouter;
