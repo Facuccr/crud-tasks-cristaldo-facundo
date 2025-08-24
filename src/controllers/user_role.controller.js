24f3d118c507d63640c82c458f97933fae95a1c5
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-08-24 14:33:19 -0300

    se agrego CRUD completo sin validaciones con sus respectivas rutas

--- src/controllers/user_role.controller.js
+++ src/controllers/user_role.controller.js
@@ -41,13 +41,111 @@ export const createUserRole = async (req, res) => {
   }
 };
 
+export const getUserRoleById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const relation = await UserRole.findByPk(id, {
+      where: { id },
+      attributes: {
+        exclude: ["user_id", "role_id"],
+      },
+
+      include: [
+        {
+          model: user_model,
+          as: "user",
+          attributes: { exclude: ["password", "id"] },
+        },
+        {
+          model: role_model,
+          as: "role",
+          attributes: { exclude: ["id"] },
+        },
+      ],
+    });
+    if (!relation) {
+      return res
+        .status(404)
+        .json({ message: "Relacion usuario-rol no encontrada" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al obtener la relacion usuario-rol",
+      error: error.message,
+    });
+  }
+};
+
 export const getAllUserRoles = async (req, res) => {
   try {
-    const roles = await UserRole.findAll();
+    const roles = await UserRole.findAll({
+      attributes: {
+        exclude: ["user_id", "role_id"],
+      },
+      include: [
+        {
+          model: user_model,
+          as: "user",
+          attributes: {
+            exclude: ["password", "id"],
+          },
+        },
+        {
+          model: role_model,
+          as: "role",
+          attributes: {
+            exclude: ["id"],
+          },
+        },
+      ],
+    });
+
     res.json(roles);
   } catch (error) {
     res
       .status(500)
       .json({ message: "error al obtener los usuarios", error: error.message });
+  }
+};
+export const updateUserRole = async (req, res) => {
+  try {
+    const user_role = await UserRole.findByPk(req.params.id);
+    if (user_role) {
+      await UserRole.update(req.body, { where: { id: req.params.id } });
+      return res.status(201).json({
+        message: "actualizado correcteamnte",
+      });
+    } else {
+      return res.status(400).json({
+        message: "el id proporcionado no existe en la base de datos",
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      message: "ocurrio un error en el servidor",
+      err: err.message,
+    });
+  }
+};
+
+export const deleteUserRole = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const relation = await UserRole.findOne({ where: { id } });
+
+    if (!relation) {
+      return res
+        .status(404)
+        .json({ message: "realcion usuario.rol no encontrada" });
+    }
+
+    await relation.destroy();
+
+    res.json({ message: "relacion usuario-rol eliminada exitosamente" });
+  } catch (error) {
+    res.status(500).json({
+      message: "error al eliminar la relacion",
+      error: error.message,
+    });
   }
 };
--- src/routes/user_roles.routes.js
+++ src/routes/user_roles.routes.js
@@ -2,12 +2,17 @@ import { Router } from "express";
 import {
   getAllUserRoles,
   createUserRole,
+  deleteUserRole,
+  updateUserRole,
+  getUserRoleById,
 } from "../controllers/user_role.controller.js";
 
 const user_roleRouter = Router();
 
 user_roleRouter.post("/", createUserRole);
-
 user_roleRouter.get("/", getAllUserRoles);
+user_roleRouter.delete("/:id", deleteUserRole);
+user_roleRouter.put("/:id", updateUserRole);
+user_roleRouter.get("/:id", getUserRoleById);
 
 export default user_roleRouter;
