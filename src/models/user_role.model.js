ccd6c82d7d5fddd8d3eb9f40762c176cf00ff3c3
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-08-25 10:21:02 -0300

    subiendo todos los archivos

--- src/models/user_role.model.js
+++ src/models/user_role.model.js
@@ -4,6 +4,7 @@ import sequelize from "../config/database.js";
 export const UserRole = sequelize.define(
   "user_roles",
   {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     user_id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
