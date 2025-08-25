69b026e1bed9cb14088f8f260bec7cc91c86f4e6
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-08-25 10:20:06 -0300

    se modificaron las rutas para que sea compatible con las validaciones

--- src/routes/role.routes.js
+++ src/routes/role.routes.js
@@ -1,9 +1,26 @@
 import { Router } from "express";
-import { createRole, getAllRoles } from "../controllers/role.controller.js";
+import {
+  createRole,
+  getAllRoles,
+  deleteRole,
+  updateRole,
+  getRoleById,
+} from "../controllers/role.controller.js";
+
+import {
+  createRoleValidations,
+  updateRoleValidations,
+  roleIdParamValidation,
+} from "../middlewares/validations/role.validations.js";
+
+import { applyValidations } from "../middlewares/validations.js";
 
 const roleRouter = Router();
 
-roleRouter.post("/", createRole);
 roleRouter.get("/", getAllRoles);
+roleRouter.get("/:id", roleIdParamValidation, applyValidations, getRoleById);
+roleRouter.post("/", createRoleValidations, applyValidations, createRole);
+roleRouter.put("/:id", updateRoleValidations, applyValidations, updateRole);
+roleRouter.delete("/:id", roleIdParamValidation, applyValidations, deleteRole);
 
 export default roleRouter;
--- src/routes/user_roles.routes.js
+++ src/routes/user_roles.routes.js
@@ -7,12 +7,41 @@ import {
   getUserRoleById,
 } from "../controllers/user_role.controller.js";
 
+import {
+  createUserRoleValidation,
+  updateUserRoleValidation,
+  getUserRoleByIdValidation,
+  deleteUserRoleValidation,
+} from "../middlewares/validations/user.role.validations.js";
+
+import { applyValidations } from "../middlewares/validations.js";
+
 const user_roleRouter = Router();
 
-user_roleRouter.post("/", createUserRole);
 user_roleRouter.get("/", getAllUserRoles);
-user_roleRouter.delete("/:id", deleteUserRole);
-user_roleRouter.put("/:id", updateUserRole);
-user_roleRouter.get("/:id", getUserRoleById);
+user_roleRouter.get(
+  "/:id",
+  getUserRoleByIdValidation,
+  applyValidations,
+  getUserRoleById
+);
+user_roleRouter.post(
+  "/",
+  createUserRoleValidation,
+  applyValidations,
+  createUserRole
+);
+user_roleRouter.put(
+  "/:id",
+  updateUserRoleValidation,
+  applyValidations,
+  updateUserRole
+);
+user_roleRouter.delete(
+  "/:id",
+  deleteUserRoleValidation,
+  applyValidations,
+  deleteUserRole
+);
 
 export default user_roleRouter;
